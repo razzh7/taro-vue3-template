870a7c5582ee16469f1b2561fb88d08286feb5de
Razzh <136013779@qq.com>
2022-06-18 20:52:15 +0800

    fix: 假装修了bug

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -11,7 +11,7 @@ import './index.scss'
 export default {
   setup() {
     const msg = ref('调试changelog8')
-    const bug = ref('假装修了一个bug')
+    const bug = ref('假装修了一个bug2')
     return {
       msg,
       bug

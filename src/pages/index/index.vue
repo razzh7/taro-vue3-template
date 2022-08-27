b5447ce1b269d8b479b8d444c3a11d1c8633c0ac
Razzh <136013779@qq.com>
2022-08-27 22:12:06 +0800

    chore: 样式调整

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -6,10 +6,10 @@ import MyButton from '@/components/Button.vue'
 <template>
   <MyDialog></MyDialog>
   <view class="text-center">
-    <view class="mb-5">按钮组件</view>
+    <view class="mb-5px">按钮组件</view>
     <MyButton></MyButton>
   </view>
-  <view class="mt-2 text-center">
+  <view class="mt-10px text-center">
     <text class="text-blue/500">UnoCss样式</text>
   </view>
 </template>

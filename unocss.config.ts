8ac7d826b674cabbbf9bb4af10310916b94d7669
Razzh <136013779@qq.com>
2022-08-27 21:41:26 +0800

    chore: unocss尺寸单位转换

--- unocss.config.ts
+++ unocss.config.ts
@@ -5,7 +5,14 @@ export default defineConfig({
     presetWeapp({
       isH5: process.env.TARO_ENV === 'h5',
       platform: 'taro',
-      taroWebpack: 'webpack5'
+      taroWebpack: 'webpack5',
+      designWidth: 375,
+      deviceRatio: {
+        640: 2.34 / 2,
+        750: 1,
+        828: 1.81 / 2,
+        375: 2 / 1
+      }
     })
   ]
 })

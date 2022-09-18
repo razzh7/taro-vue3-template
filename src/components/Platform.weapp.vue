1150c11dcc2cd665012bccf4331e953f8d23e5c4
Razzh <136013779@qq.com>
2022-09-18 23:46:03 +0800

    feat: add Taro h5/weapp 组件

---
+++ src/components/Platform.weapp.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <view class="text-center text-indigo"> Im' Weapp Platform Component! </view>
+</template>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
-import MyDialog from '@/components/Dialog.vue'
-import MyButton from '@/components/Button.vue'
+import MyDialog from '@/components/Dialog'
+import MyButton from '@/components/Button'
+import PlatFormComponent from '@/components/Platform'
 </script>
 
 <template>
   <view>
     <MyDialog></MyDialog>
     <view class="text-center">
-      <view class="mb-5px">按钮组件</view>
+      <view class="mb-5px text-fuchsia">按钮组件</view>
       <MyButton></MyButton>
     </view>
     <view class="mt-10px text-center">
       <text class="text-blue/500">UnoCss样式</text>
+    </view>
+    <view class="text-center text-fuchsia">
+      <view class="mb-5px">多平台组件</view>
+      <PlatFormComponent />
     </view>
   </view>
 </template>
